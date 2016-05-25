8b2cf84b369e6d4e81c61ae9c2acdcdfc21f205b
zhenfei <zhenfei@ubuntu.ubuntu-domain>
2016-05-25 13:14:34 -0400

    vs 2015 build

---
+++ src/randombytes-vs.c
@@ -0,0 +1,60 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <windows.h>
+#include <Wincrypt.h>
+#include "randombytes.h"
+
+
+
+void randombytes(unsigned char *x, unsigned long long xlen)
+{
+	int i;
+	srand(time(NULL) - 3);
+	HCRYPTPROV hCryptProv = NULL;
+
+	if (CryptAcquireContext(&hCryptProv,               // handle to the CSP
+		NULL,                  // container name
+		NULL,                      // use the default provider
+		PROV_RSA_FULL,             // provider type
+		0))                        // flag values
+		;
+	else
+	{
+		if (GetLastError() == NTE_BAD_KEYSET)
+		{
+			if (CryptAcquireContext(
+				&hCryptProv,
+				NULL,
+				NULL,
+				PROV_RSA_FULL,
+				CRYPT_NEWKEYSET))
+				;
+			else
+			{
+				printf("Could not create a new key container.\n");
+				exit(1);
+			}
+		}
+		else
+		{
+			printf("A cryptographic service handle could not be "
+				"acquired.\n");
+			exit(1);
+		}
+
+	} // End of else.
+	if (CryptGenRandom(hCryptProv, xlen, x))
+		;
+	else
+	{
+		printf("Error during CryptGenRandom.\n");
+		exit(1);
+	}
+	printf("window random string: ");
+	for (i = 0; i < xlen; i++)
+		printf("%2x ", x[i]);
+	printf("\n");
+	Sleep(1);
+}
